3fab34fee5ef28069fd0a0adcebd60a964004c2e
Diwank Singh <diwank.singh@gmail.com>
2017-03-25 19:35:05 -0400

    Changed predicates
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- src/Predicates.hs
+++ src/Predicates.hs
@@ -1,10 +1,9 @@
-module Predicates ( checkFinished, checkWin, getScore, getState, validPos )  where
+module Predicates ( checkFinished, checkWin, getScore, validPos )  where
 
 -- Imports
 import qualified Prelude as P
-import           Prelude ((++), (!!), map, and, or, quot)
+import           Prelude (map, and, or)
 import           Foundation
-import           System.Random
 
 
 -- Local imports
@@ -22,27 +21,13 @@ checkWin c game = or (map isComplete lines)
     lines = getLines game
     isComplete = and . (map (==c))
 
-getState :: Cell -> Grid -> State
-getState player game = if lost then LOST else if won then WON else DRAW
-  where
-    lost = checkWin (opponent player) game
-    won = checkWin player game
-
-    opponent X = O
-    opponent O = X
-    opponent NULL = NULL
-
 validPos :: Int -> Bool
 validPos = between 1 grid_sq
 
-getScore :: Cell -> Depth -> Grid -> Maybe Score
-getScore player depth game = case checkFinished game of
-  True -> Just $ scoreSign * (absScore - depth)
+getScore :: Depth -> Grid -> Maybe Score
+getScore depth game = case checkFinished game of
+  True -> Just $ score - depth
   _ -> Nothing
   
   where
-    score = fromEnum $ getState player game
-    absScore = abs score
-    scoreSign = if score == 0
-                  then 0
-                  else score `quot` absScore
+    score = fromEnum $ checkWin computer_cell game
