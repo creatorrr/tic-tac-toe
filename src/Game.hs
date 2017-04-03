819c6a95de61b2b20b36938ea9098aab24ff4c85
Diwank Singh <diwank.singh@gmail.com>
2017-04-03 10:56:50 -0400

    Cleaned up Game.hs
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- src/Game.hs
+++ src/Game.hs
@@ -3,18 +3,13 @@ module Game
   ) where
 
 -- Local imports
-import Constants
-import Predicates
 import Types
 import Utils
 
 -- Game functions
 play :: Grid -> Pos -> Grid
-play game pos =
-  if exists
-    then error "Invalid move"
-    else (take i game) ++ [nextTurn game] ++ (drop pos game)
+play game pos
+  | exists pos = error "Invalid move"
+  | otherwise = take (decc pos) game ++ [nextTurn game] ++ drop pos game
   where
-    i = pos - 1
-    existing = game !! i
-    exists = existing /= NULL
+    exists = (/= NULL) . (!!) game . decc
